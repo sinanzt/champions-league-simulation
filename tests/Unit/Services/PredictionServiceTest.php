26be8b04fb6e45770b0ec7db926459af784e42df
sinanzt <sinanozata44@gmail.com>
2024-05-17 09:10:45 +0300

    Removed comments

--- tests/Unit/Services/PredictionServiceTest.php
+++ tests/Unit/Services/PredictionServiceTest.php
@@ -20,11 +20,9 @@ class PredictionServiceTest extends TestCase
         $teamPower = 80;
         $opponentPower = 70;
 
-        // Test when the team is host
         $isHost = true;
         $hostWinProbability = $this->predictionService->calculateWinProbability($teamPower, $opponentPower, $isHost);
         
-        // Test when the team is not host
         $isHost = false;
         $guestWinProbability = $this->predictionService->calculateWinProbability($teamPower, $opponentPower, $isHost);
 
@@ -37,7 +35,6 @@ class PredictionServiceTest extends TestCase
         $teamPower = 80;
         $winProbability = 0.6;
 
-        // Test for a reasonable number of goals
         $goals = $this->predictionService->predictGoals($teamPower, $winProbability);
         $this->assertGreaterThanOrEqual(0, $goals);
         $this->assertLessThanOrEqual(10, $goals);
